12dd2a46d7672ea342b2c612585e9c4d4d212e1f
terry623 <terry92516@gmail.com>
2018-04-17 04:09:04 +0800

    add flat ui

--- content.js
+++ content.js
@@ -1,8 +1,26 @@
-document.body.onload = addElement;
+// document.body.onload = addElement;
 
-function addElement() {
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode("This is test!");
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv);
+// function addElement() {
+//   var newDiv = document.createElement("div");
+//   var newContent = document.createTextNode("This is test!This is test!This is test!This is test!");
+//   newDiv.appendChild(newContent);
+//   document.html.body.div[2].appendChild(newDiv);
+// }
+
+// $(
+//   "body > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(5) > td > input:nth-child(1)"
+// ).replaceWith(
+//   `<input type="submit" name="Submit" value="登入" class="btn btn-primary bottom_class">Primary</button>`
+// );
+
+console.log("Append...");
+
+$(document).ready(function() {
+  console.log("Please!");
+  var t = setTimeout(change, 1000);
+});
+
+function change() {
+  console.log("Change Color!!");
+  $("html > frameset").css("bordercolor", "red");
 }
