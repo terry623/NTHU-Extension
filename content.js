253899642464c9c45d1f71a5a86c5baf432deb01
terry623 <terry92516@gmail.com>
2018-04-17 14:58:41 +0800

    change tilte (but now can't detect user click other page)

--- content.js
+++ content.js
@@ -1,26 +1,16 @@
-// document.body.onload = addElement;
-
-// function addElement() {
-//   var newDiv = document.createElement("div");
-//   var newContent = document.createTextNode("This is test!This is test!This is test!This is test!");
-//   newDiv.appendChild(newContent);
-//   document.html.body.div[2].appendChild(newDiv);
+console.log("Append...");
+setInterval(function() {
+  change_txt();
+}, 1000);
+// function make_side_red() {
+//   var target = window.frames[1]["document"]["body"];
+//   $(target)
+//     .find()
+//     .after();
 // }
 
-// $(
-//   "body > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(5) > td > input:nth-child(1)"
-// ).replaceWith(
-//   `<input type="submit" name="Submit" value="登入" class="btn btn-primary bottom_class">Primary</button>`
-// );
-
-console.log("Append...");
-
-$(document).ready(function() {
-  console.log("Please!");
-  var t = setTimeout(change, 1000);
-});
-
-function change() {
-  console.log("Change Color!!");
-  $("html > frameset").css("bordercolor", "red");
+function change_txt() {
+  console.log("Change Text...");
+  var target = window.frames[2]["document"]["body"];
+  $("div > p:first()", target).text("拜託嗚嗚嗚!!");
 }
