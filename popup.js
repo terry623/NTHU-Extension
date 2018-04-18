597dcb13e15ef7688731e01fd0e5456646a7e72d
terry623 <terry92516@gmail.com>
2018-04-19 00:49:17 +0800

    finish popup jquery

---
+++ popup.js
@@ -0,0 +1,8 @@
+//   chrome.tabs.executeScript({
+//     file: "alert.js"
+//   });
+
+$("#clickme").click(function() {
+  console.log("Click Button");
+  $("#menu").modal("show");
+});
--- src/content.js
+++ src/content.js
@@ -1,50 +1,15 @@
 import { getUrlParameter } from "./help";
 import { main } from "./data";
-import jQuery from "jquery";
-window.$ = window.jQuery = jQuery;
 
 var acix = getUrlParameter("ACIXSTORE");
 console.log("ACIXSTORE is " + acix);
 
-setTimeout(function() {
-  addCss();
-  change();
-}, 15000);
+// setTimeout(function() {
+//   change();
+// }, 10000);
 
-setTimeout(function() {
-  console.log("Show Modal...");
-  var body = window.frames[2]["document"]["body"];
-  $(".ui.modal", body).modal("show");
-}, 20000);
-
-function addCss() {
-  console.log("Add Css...");
-  var head = window.frames[2]["document"]["head"];
-
-  var script = document.createElement("script");
-  script.src = chrome.extension.getURL("semantic/dist/semantic.min.js");
-  $("link:first", head).before(script);
-
-  // var jq = document.createElement("script");
-  // jq.src = "https://code.jquery.com/jquery-3.1.1.min.js";
-  // jq.integrity = "sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=";
-  // jq.crossorigin = "anonymous";
-  // $("link:first", head).before(jq);
-
-  var link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.type = "text/css";
-  link.href = chrome.extension.getURL("semantic/dist/semantic.min.css");
-  $("script:first", head).before(link);
-}
-
-// function removeBackground() {
-//   var doc = window.frames[2]["document"];
-//   $("body", doc).removeAttr("background");
+// function change() {
+//   console.log("Change...");
+//   var document = window.frames[2]["document"];
+//   $("body", document).append(main);
 // }
-
-function change() {
-  console.log("Change...");
-  var document = window.frames[2]["document"];
-  $("body", document).append(main);
-}
