1227051e28f1377851eabe459dce3002a19894c9
terry623 <terry92516@gmail.com>
2018-04-19 22:37:06 +0800

    finish basic ui in main page

--- dist/popup.bundle.js
+++ dist/popup.bundle.js
@@ -60,13 +60,17 @@
 
 	"use strict";
 	
-	//   chrome.tabs.executeScript({
-	//     file: "alert.js"
-	//   });
+	$(".ui.dropdown").dropdown();
 	
 	$("#clickme").click(function () {
 	  console.log("Click Button");
 	  $("#testModal").modal("show");
+	});
+	
+	$(".ui.menu").on("click", ".item", function () {
+	  if (!$(this).hasClass("dropdown")) {
+	    $(this).addClass("active").siblings(".item").removeClass("active");
+	  }
 	});
 
 /***/ })
--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,15 @@
-//   chrome.tabs.executeScript({
-//     file: "alert.js"
-//   });
+$(".ui.dropdown").dropdown();
 
 $("#clickme").click(function() {
   console.log("Click Button");
   $("#testModal").modal("show");
+});
+
+$(".ui.menu").on("click", ".item", function() {
+  if (!$(this).hasClass("dropdown")) {
+    $(this)
+      .addClass("active")
+      .siblings(".item")
+      .removeClass("active");
+  }
 });
