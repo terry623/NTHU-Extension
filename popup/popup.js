0f0bbced86d4ecbd301424023677c70921b2c5fe
terry623 <terry92516@gmail.com>
2018-04-22 13:22:38 +0800

    can get course information

--- popup/popup.js
+++ popup/popup.js
@@ -4,8 +4,8 @@ var request = require("request");
 import { getUrlVars } from "./helper";
 
 $.fn.api.settings.api = {
-  "get user":
-    "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/4/4.19/JH4j002.php?ACIXSTORE={ACIXSTORE}"
+  // "get user":
+  //   "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/4/4.19/JH4j002.php?ACIXSTORE={ACIXSTORE}"
 };
 
 $.fn.api.settings.successTest = function(response) {
@@ -15,84 +15,96 @@ $.fn.api.settings.successTest = function(response) {
   return false;
 };
 
+function getUserName(acix) {
+  request(
+    {
+      url:
+        "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/4/4.19/JH4j002.php?ACIXSTORE=" +
+        acix,
+      encoding: null
+    },
+    function(err, response, body) {
+      if (!err && response.statusCode == 200) {
+        var str = iconv.decode(new Buffer(body), "big5");
+        var temp = document.createElement("div");
+        temp.innerHTML = str;
+        var htmlObject = temp.firstChild;
+        var found = $(
+          "div > form > table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(4)",
+          temp
+        );
+
+        var welcome = "<div>Hi~ " + found.text() + " !</div>";
+        $("#user").prepend(welcome);
+        $(".content_item.homePage").show();
+      }
+    }
+  );
+}
+
+function getCourseInfo(acix, course_no) {
+  request(
+    {
+      url:
+        "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/common/Syllabus/1.php?ACIXSTORE=" +
+        acix +
+        "&c_key=" +
+        course_no,
+      encoding: null
+    },
+    function(err, response, body) {
+      if (!err && response.statusCode == 200) {
+        var str = iconv.decode(new Buffer(body), "big5");
+        var temp = document.createElement("div");
+        temp.innerHTML = str;
+        var htmlObject = temp.firstChild;
+
+        var no = $(
+          "div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(2)",
+          temp
+        );
+        var name_zh = $(
+          "div > table:nth-child(1) > tbody > tr:nth-child(3) > td.class3",
+          temp
+        );
+
+        var name_en = $(
+          "div > table:nth-child(1) > tbody > tr:nth-child(4) > td.class3",
+          temp
+        );
+
+        $("#no").text(no.text());
+        $("#course_name_zh").text(name_zh.text() + " " + name_en.text());
+      }
+    }
+  );
+}
+
 $(document).ready(function() {
   $(".content_item").hide();
 
-  var username;
-  var acix;
   chrome.tabs.query(
     { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
     function(tabs) {
-      acix = getUrlVars(tabs[0].url)["ACIXSTORE"];
+      var acix = getUrlVars(tabs[0].url)["ACIXSTORE"];
       console.log("ACIXSTORE is " + acix);
+      getUserName(acix);
 
-      request(
-        {
-          url:
-            "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/4/4.19/JH4j002.php?ACIXSTORE=" +
-            acix,
-          encoding: null
-        },
-        function(err, response, body) {
-          if (!err && response.statusCode == 200) {
-            var str = iconv.decode(new Buffer(body), "big5");
-            var temp = document.createElement("div");
-            temp.innerHTML = str;
-            var htmlObject = temp.firstChild;
-            var found = $(
-              "div > form > table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(4)",
-              temp
-            );
-
-            var welcome = "<div>Hi~ " + found.text() + " !</div>";
-            $("#user").prepend(welcome);
-            $(".content_item.homePage").show();
-          }
-        }
-      );
-
-      // $("#user")
-      //   .api({
-      //     action: "get user",
-      //     on: "now",
-      //     urlData: {
-      //       ACIXSTORE: acix
-      //     },
-      //     onResponse: function(response) {
-      //       // make some adjustments to response
-      //       console.log(response);
-      //       return response;
-      //     },
-      //     successTest: function(response) {
-      //       // test whether a JSON response is valid
-      //       return response.success || false;
-      //     },
-      //     onComplete: function(response) {
-      //       // make some adjustments to response
-      //       console.log(response);
-      //       console.log("---------------------");
-      //       // var buf = iconv.encode(response, "Big5");
-      //       var str = decodeURIComponent(response);
-      //       var buf = iconv.decode(new Buffer(str), "Big5");
-      //       // var str = iconv.decode(new Buffer(response), "big5");
-      //       // var str = decodeURIComponent(response);
-      //       console.log(buf);
-      //     }
-      //   })
-      //   .text("NOTYET");
+      const course_no = "10620CS  135500";
+      getCourseInfo(acix, course_no);
     }
   );
 });
 
 $(".ui.dropdown").dropdown();
+$(".shape").shape();
 
-$("#clickme").click(function() {
-  console.log("Click Button");
-  $("#testModal").modal("show");
+$("#clicktoflip").click(function() {
+  $(".shape").shape("flip right");
 });
 
 $(".ui.menu").on("click", ".item", function() {
-  if (!$(this).hasClass("dropdown")) {
+  if (!$(this).hasClass("dropdown") && !$(this).hasClass("istable")) {
     $(this)
       .addClass("active")
       .siblings(".item")
@@ -105,5 +117,6 @@ $(".ui.menu").on("click", ".item", function() {
     else if ($(this).hasClass("searchPage")) t.not(".searchPage").hide();
     else if ($(this).hasClass("choosePage")) t.not(".choosePage").hide();
     else if ($(this).hasClass("recommendPage")) t.not(".recommendPage").hide();
+    else if ($(this).hasClass("singlePage")) t.not(".singlePage").hide();
   }
 });
