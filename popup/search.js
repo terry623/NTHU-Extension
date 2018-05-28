89e83d7e4ccaba7ad42e4644469fab61f34ca25b
terry623 <terry92516@gmail.com>
2018-05-28 20:45:12 +0800

    remove some useless file

--- popup/search.js
+++ popup/search.js
@@ -2,23 +2,6 @@ import { getCourseInfo } from "./api";
 import { translateTopic } from "./helper";
 import { checkConflict } from "./conflict";
 var request = require("request");
-var elasticsearch = require("elasticsearch");
-var client = new elasticsearch.Client({
-  host: "http://localhost:9200"
-});
-client.ping(
-  {
-    // ping usually has a 3000ms timeout
-    requestTimeout: 1000
-  },
-  function(error) {
-    if (error) {
-      console.trace("Elasticsearch cluster is down");
-    } else {
-      console.log("Elasticsearch is well");
-    }
-  }
-);
 
 function renderSearchResult(hits, callback) {
   storeCourseInfo(hits);
